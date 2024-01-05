a7017bad1c744f21e420141e286e3683aedb071a
Heitor Leite Ramos <hlr@cin.ufpe.br>
2024-01-05 11:50:14 -0300

    created new tests for home scree, now it has a test with tap action and a finder with specific widgets number

--- flutter_test/test/home_test.dart
+++ flutter_test/test/home_test.dart
@@ -1,3 +1,4 @@
+import 'package:estilizacao_componentes/components/box_card.dart';
 import 'package:estilizacao_componentes/data/bank_inherited.dart';
 import 'package:estilizacao_componentes/screens/home.dart';
 import 'package:flutter/material.dart';
@@ -27,7 +28,7 @@ void main() {
     );
     expect(find.byType(LinearProgressIndicator), findsOneWidget);
   });
-    testWidgets('Find an AccountStatus', (tester) async {
+  testWidgets('Find an AccountStatus', (tester) async {
     await tester.pumpWidget(
       MaterialApp(
         home: BankInherited(
@@ -36,5 +37,38 @@ void main() {
       ),
     );
     expect(find.byKey(Key('testKey')), findsOneWidget);
+  });
+
+  testWidgets('Find 5 BoxCards', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BankInherited(
+          child: Home(),
+        ),
+      ),
+    );
+    expect(find.byWidgetPredicate((widget) {
+      if (widget is BoxCard) {
+        return true;
+      } else {
+        return false;
+      }
+    }), findsNWidgets(5));
+  });
+  testWidgets('When tap deposit should upload earned in 10', (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BankInherited(
+          child: Home(),
+        ),
+      ),
+    );
+    //changing screen values
+    await tester.tap(find.text('Deposit'));
+    await tester.tap(find.text('Earned'));
+
+    //updating or redrawing the screen
+    await tester.pumpAndSettle();
+    expect(find.text('\$10.0'), findsOneWidget);
   });
 }
